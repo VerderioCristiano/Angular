3a3feb9aebd404209a5d6bfb6c180c250de9cf2f
Cristiano Verderio <verderio.cristiano@iisgalvanimi.edu.it>
2024-02-01 13:10:49 +0000

    es 3 completato

--- esercizio3/src/app/terzo-componente/terzo-componente.component.ts
+++ esercizio3/src/app/terzo-componente/terzo-componente.component.ts
@@ -1,19 +1,39 @@
 import { Component } from '@angular/core';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'terzo-componente',
   standalone: true,
-  imports: [MatProgressBarModule],
+  imports: [MatProgressBarModule, MatButtonModule],
   templateUrl: './terzo-componente.component.html',
   styleUrl: './terzo-componente.component.css'
 })
 export class TerzoComponenteComponent {
-likes:number=10;
+likes:number=0;
+constructor(){
+
+this.likes = Math.floor(Math.random() * 100 );
+this.likeChecker();
+}
+
+likeChecker():string{
+if(this.likes<33 ){
+  return "primary";
+}
+  else if(this.likes>=33 && this.likes<66){
+    return "accent";
+  }
+  else{
+    return "warn";
+  }
+}
 
 
 
 clickBottone(){
-  this.likes = this.likes + 1
+  this.likes = this.likes + 1;
+  this.likeChecker();
 }
+
 }
