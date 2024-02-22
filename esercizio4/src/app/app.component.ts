3afeececc1fbaef024612c3cbf2dba4953b21d08
Cristiano Verderio <verderio.cristiano@iisgalvanimi.edu.it>
2024-02-22 13:53:37 +0000

    boh

--- esercizio4/src/app/app.component.ts
+++ esercizio4/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
